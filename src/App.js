0e01d860310c05ec75ddcee49e8b29cf841abcd2
Howard Wu <hwu106@ucsc.edu>
2023-05-04 23:01:10 -0700

    comit

--- src/App.js
+++ src/App.js
@@ -6,34 +6,13 @@ import { BrowserRouter as Router, Link } from 'react-router-dom'
 import Dashboard from './dashboard'
 
 function App() {
-  const isLoggedIn =window.localStorage.getItem("loggedIn");
+  // const isLoggedIn =window.localStorage.getItem("loggedIn");
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <Dashboard/>
+          <Dashboard />
         </header>
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={'/sign-in'}>
-              SocialHub
-            </Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-in'}>
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-up'}>
-                    Sign up
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
       </div>
     </Router>
   )
