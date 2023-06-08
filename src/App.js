4dbd9bd40f7d3a7ab746950a864b85714729d88f
Howard Wu <hwu106@ucsc.edu>
2023-06-07 18:41:53 -0700

    working demo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import Landing from "./pages/landing"
-import Login from "./pages/login"
 import Youtube from "./pages/youtube"
 import MainDashboard from "./pages/dashboard"
 import Facebook from './pages/facebook'
@@ -13,7 +12,6 @@ function App() {
     <Router>
       <Routes>
         <Route exact path="/" Component={Landing}></Route>
-        <Route exact path="/login" Component={Login}></Route>
         <Route exact path="/dashboard_yt" Component={Youtube}></Route>
         <Route exact path="/MainDashboard" Component={MainDashboard}></Route>
         <Route exact path="/dashboard_fb" Component={Facebook}></Route>
